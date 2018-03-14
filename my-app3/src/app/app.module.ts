c4fdbfb5aa18efb727a5a04e04ed9f2ec72a2aa7
yyccQQu <643929860@qq.com>
2018-03-14 17:54:00 +0800

    first
    one

---
+++ my-app3/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule }    from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+
+//import { AppRoutingModule }     from './app-routing.module';
+
+import { AppComponent } from './app.component';
+//import { DashboardComponent }   from './dashboard/dashboard.component';
+//import { HeroesComponent }      from './heroes/heroes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
